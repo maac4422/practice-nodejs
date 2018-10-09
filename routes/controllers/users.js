6e90888400c5f4bc2167716132d9120bf5fb2182
maac4422 <miguelarenas98@hotmail.com>
2018-10-09 08:35:37 -0500

    Connect with mongo DB

--- routes/controllers/users.js
+++ routes/controllers/users.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+const {mongoose} = require('../../db/mongoose')
+const {User} = require('../../db/models/user')
 //const router = express.Router()
 
 /* GET users listing. */
 
 exports.getUsers = function (req, res, next) {
-    res.json({users: [{name: 'Timmy'}]})
+    User.find().then((users) => {
+        res.send({users})
+    },(e) => {
+        res.status(400).send(e)
+    })
 }
 
 //module.exports = router
