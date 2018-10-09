9b43b661230dbb01f92ed2c342c6928d2afa2879
maac4422 <miguelarenas98@hotmail.com>
2018-10-09 09:50:14 -0500

    Config CORS

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const cors = require('cors')
 
 const app = express()
 const corsOptions = {
-    origin: true,
-    credentials: true
+    origin: "*",
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
 }
 
 app.use(cors(corsOptions))
