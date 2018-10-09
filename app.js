730fabaae2c1664a52e2fec60ba1791197203375
maac4422 <miguelarenas98@hotmail.com>
2018-10-09 08:15:24 -0500

    Config cors

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const app = express()
+const corsOptions = {
+    origin: true,
+    credentials: true
+}
 
+app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
