048dbdc763a70ca55e7bbc19ee6cfbfd254dbfc4
maac4422 <miguelarenas98@hotmail.com>
2018-10-16 10:35:48 -0500

    Create conneconnection to websocket

---
+++ sockets/handlers.js
@@ -0,0 +1,14 @@
+function makeHandleEvent(client, clientManager, chatroomManager) {
+    function ensureExists(getter, rejectionMessage) {
+      return new Promise(function (resolve, reject) {
+        const res = getter()
+        return res
+          ? resolve(res)
+          : reject(rejectionMessage)
+      })
+    }
+}
+
+module.exports = function (client, clientManager, chatroomManager) {
+
+}
